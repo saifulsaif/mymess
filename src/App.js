c3601e060a2bbb271d949d4822b410d1673af574
saifulsaif <saifulsaif5854@gmail.com>
2022-06-13 01:04:51 +0600

    state, event , hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
 
-const Person = (props) =>{
-  return (
-   <>
-   <h1>Name : {props.name}</h1>
-   <h2>Last Name : {props.last_name} </h2>
-   <h2>Age : {props.age} </h2>
-   </>
-  )
-}
 
 const App = () => {
-  const name = 'saif';
+  const [counter, setCounter] = useState(0);
+
+  useEffect (()=>{
+     setCounter(100);
+  });
 
   return (
     <div className="App">
-       <Person name={'Rony'} last_name={'Kobir'} age={'23'} />
-       <Person name='Joy' last_name='Khab' age='43' />
+      
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}> + </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}> - </button>
 
     </div>
   );
