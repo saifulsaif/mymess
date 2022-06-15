f68a81793793340d9ae76184d564b77199db792d
saifulsaif <saifulsaif5854@gmail.com>
2022-06-15 23:57:42 +0600

    get multi items via API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 
 import './App.css';
@@ -8,44 +8,60 @@ import searchIcon from './search.svg';
 
 const API_URL = 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
 const movie1 = {
- "image" : "https://s3.amazonaws.com/donovanbailey/products/api_featured_images/000/000/495/original/open-uri20171223-4-9hrto4?1514063330",
- "year" :"2022",
- "title" : "Amazing action movie"
+  "image": "https://s3.amazonaws.com/donovanbailey/products/api_featured_images/000/000/495/original/open-uri20171223-4-9hrto4?1514063330",
+  "year": "2022",
+  "title": "Amazing action movie"
 };
 const App = () => {
-    const searchMovies = async(title) => {
-      const response = await fetch(`${API_URL}&s=${title}`);
-      const data = await response.json();
-      console.log(data);
-    }
-    useEffect(() => {
-      searchMovies('spidermen');
-    }, []);
+  const [movies, setMovies] = useState([]);
 
-    return (
-        <div className="app">
-            <h1>Movie Home</h1>
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data[0]);
+    setMovies(data);
+  }
+  useEffect(() => {
+    searchMovies('spidermen');
+  }, []);
 
-            <div className="search">
-                <input
-                placeholder="Search for movie"
-                value="Superman"
-                onChange={() => {}}
-                />
-                <img
-                src={searchIcon}
-                alt="search"
-                onChange={() => {
+  return (
+    <div className="app">
+      <h1>Products Home</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movie"
+                 value="Superman"
+          onChange={() => { }}
+        />
+        <img
+          src={searchIcon}
+          alt="search"
+          onChange={() => {
 
-                }}
-                />
+          }}
+        />
+      </div>
+
+
+      {
+        movies?.length > 0
+          ? (
+            <div className="container">
+              {movies.map((movie) => (
+                <MovieCard movie1={movie} />
+              ))}
             </div>
-             <div className="container">
-              <MovieCard movie1={movie1} />
-             </div>
+          ) : (
+            <div className="empty">
+              <h2>No Product found</h2>
+            </div>
+          )
+      }
 
-        </div>
-    );
+
+    </div>
+  );
 }
 
 export default App;
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -5,16 +5,16 @@ const MovieCard = ( {movie1} ) =>{
         <div className="movie">
             <div>
                 <p>
-                    {movie1.year}
+                    {movie1.price}
                 </p>
             </div>
 
             <div>
-                <img src={movie1.image} alt={movie1.title}/>
+                <img src={movie1.image_link} alt={movie1.name}/>
             </div>
             <div>
                 <h3>
-                    {movie1.title}
+                    {movie1.name}
                 </h3>
             </div>
        </div>
