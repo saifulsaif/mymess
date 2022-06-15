ee109ecd670596cf3b5d3422dba399778a89e365
saifulsaif <saifulsaif5854@gmail.com>
2022-06-16 00:15:14 +0600

    add search event

--- src/App.js
+++ src/App.js
@@ -14,15 +14,16 @@ const movie1 = {
 };
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
+  const searchMovies = async (name) => {
+    const response = await fetch(`${API_URL}&s=${name}`);
     const data = await response.json();
-    console.log(data[0]);
+    console.log(name);
     setMovies(data);
-  }
+  };
   useEffect(() => {
-    searchMovies('spidermen');
+    searchMovies("spidermen");
   }, []);
 
   return (
@@ -31,35 +32,27 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movie"
-                 value="Superman"
-          onChange={() => { }}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img
           src={searchIcon}
           alt="search"
-          onChange={() => {
-
-          }}
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
-
-      {
-        movies?.length > 0
-          ? (
-            <div className="container">
-              {movies.map((movie) => (
-                <MovieCard movie1={movie} />
-              ))}
-            </div>
-          ) : (
-            <div className="empty">
-              <h2>No Product found</h2>
-            </div>
-          )
-      }
-
-
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie1={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Product found</h2>
+        </div>
+      )}
     </div>
   );
 }
